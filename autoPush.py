1f060c342380c9d30c9ff12ad149245b8fc7a0ce
ebdeuslave <abderrahim.essaouaf@gmail.com>
2025-05-22 21:44:03 +0100

    isolate stores to .env

--- autoPush.py
+++ autoPush.py
@@ -1,7 +1,7 @@
 import os
 
 def autoPush(commitMsg):
-   print(os.system(f"git add . && git commit -m '{commitMsg}' && git push"))
+   print(os.system(f"git add . && git commit -m '{commitMsg}' && git push --force"))
    
    
 autoPush("isolate stores to .env")
